ddc97e5ea00e2ca91bcb6de39b894a4eeba8554e
Gabriel Jaldon <gjaldon85@gmail.com>
2015-07-07 20:24:36 +0800

    Package metadata

--- mix.exs
+++ mix.exs
@@ -1,31 +1,33 @@
 defmodule EctoEnum.Mixfile do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [app: :ecto_enum,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     description: "Ecto extension to support enums in models",
+     package: package,
+     name: "EctoEnum",
+     docs: [source_ref: "v#{@version}",
+            source_url: "https://github.com/gjaldon/ecto_enum"]]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type `mix help compile.app` for more information
+  defp package do
+    [contributors: ["Gabriel Jaldon"],
+     licenses: ["MIT"],
+     links: %{github: "https://github.com/gjaldon/ecto_enum"},
+     files: ~w(mix.exs README.md CHANGELOG.md lib)]
+  end
+
   def application do
     [applications: [:logger, :ecto]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
   defp deps do
     [{:ecto, "~> 0.13.1"},
      {:postgrex, "~> 0.8.3", optional: true}]
