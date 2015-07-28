3af6b854876a70c3658aaa92b122679c9f10b617
Gabriel Jaldon <gjaldon85@gmail.com>
2015-07-28 10:26:08 +0800

    Relax version requirements for Ecto. Upgrade to 2.2. Closes #2

--- mix.exs
+++ mix.exs
@@ -1,14 +1,12 @@
 defmodule EctoEnum.Mixfile do
   use Mix.Project
 
-  @version "0.2.1"
+  @version "0.2.2"
 
   def project do
     [app: :ecto_enum,
      version: @version,
      elixir: "~> 1.0",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
      deps: deps,
      description: "Ecto extension to support enums in models",
      package: package,
@@ -29,9 +27,9 @@ defmodule EctoEnum.Mixfile do
   end
 
   defp deps do
-    [{:ecto, "~> 0.13.1"},
-     {:postgrex, "~> 0.8.3", optional: true},
-     {:mariaex, "~> 0.3.0", optional: true},
+    [{:ecto, ">= 0.13.1"},
+     {:postgrex, ">= 0.8.3", optional: true},
+     {:mariaex, ">= 0.3.0", optional: true},
      {:ex_doc, "~> 0.7", only: :docs},
      {:earmark, "~> 0.1", only: :docs},
      {:inch_ex, only: :docs}]
