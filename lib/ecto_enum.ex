90eafbe95bf8f89d6f0f89005ee35d575372f67e
Gullit Miranda <gullitmiranda@gmail.com>
2016-06-21 20:45:31 -0300

    Adding support to `use`

--- lib/ecto_enum.ex
+++ lib/ecto_enum.ex
@@ -1,16 +1,18 @@
 defmodule EctoEnum do
   @moduledoc """
-  Provides `defenum/2` macro for defining an Enum Ecto type.
+  Provides `enum/1` macro to define the list of valid values.
   """
 
   @doc """
-  Defines an enum custom `Ecto.Type`.
+  Provides `enum/1` macro to define the list of valid values.
 
   It can be used like any other `Ecto.Type` by passing it to a field in your model's
   schema block. For example:
 
-      import EctoEnum
-      defenum StatusEnum, registered: 0, active: 1, inactive: 2, archived: 3
+      defmodule StatusEnum do
+        use EctoEnum
+        enum ~w(registered active inactive archived)
+      end
 
       defmodule User do
         use Ecto.Model
@@ -29,7 +31,7 @@ defmodule EctoEnum do
       iex> Repo.get(User, user.id).status
       :registered
 
-      iex> %{changes: changes} = cast(%User{}, %{"status" => "Active"}, ~w(status), [])
+      iex> %{changes: changes} = cast(%User{}, %{"status" => "active"}, ~w(status), [])
       iex> changes.status
       :active
 
@@ -38,6 +40,10 @@ defmodule EctoEnum do
 
   Passing a value that the custom Enum type does not recognize will result in an error.
 
+      iex> %{errors: errors} = cast(%User{}, %{"status" => :retroactive}, ~w(status), [])
+      iex> errors
+      [status: "is invalid"]
+
       iex> Repo.insert!(%User{status: :none})
       ** (Elixir.EctoEnum.Error) :none is not a valid enum value
 
@@ -45,7 +51,11 @@ defmodule EctoEnum do
   enum map in runtime.
 
       iex> StatusEnum.__enum_map__()
-      [registered: 0, active: 1, inactive: 2, archived: 3]
+      ["registered", "active", "inactive", "archived"]
+
+      iex> StatusEnum.__meta__()
+      %{0 => "registered", 1 => "active", 2 => "inactive", 3 => "archived",
+        "active" => 1, "archived" => 3, "inactive" => 2, "registered" => 0}
   """
 
   defmodule Error do
@@ -57,80 +67,134 @@ defmodule EctoEnum do
     end
   end
 
+
+  defmacro __using__(_opts) do
+    quote do
+      @behaviour Ecto.Type
+
+      import EctoEnum
+
+      def type, do: :integer
+
+      def cast(term) do
+        EctoEnum.cast(term, __meta__)
+      end
+
+      def load(term) do
+        EctoEnum.load(term, __meta__)
+      end
+
+      def dump(term) do
+        EctoEnum.dump(term, __meta__)
+      end
+    end
+  end
+
+  defmacro enum(enums, _opts \\ []) do
+    quote do
+      def __meta__, do: EctoEnum.enums_to_map(unquote(enums))
+      def __enum_map__, do: unquote(enums)
+    end
+  end
+
+  # compatilibidade with the previous version
   defmacro defenum(module, enum_kw) when is_list(enum_kw) do
     quote do
       kw = unquote(enum_kw) |> Macro.escape
 
       defmodule unquote(module) do
-        @behaviour Ecto.Type
+        use EctoEnum
 
         @enum_kw kw
-        @enum_map for {atom, int} <- kw, into: %{}, do: {int, atom}
-        @enum_map_string for {atom, int} <- kw, into: %{}, do: {Atom.to_string(atom), int}
-
-        def type, do: :integer
-
-        def cast(term) do
-          check_value!(term)
-          EctoEnum.cast(term, @enum_map)
-        end
-
-        def load(int) when is_integer(int) do
-          {:ok, @enum_map[int]}
-        end
-
-        def dump(term) do
-          check_value!(term)
-          EctoEnum.dump(term, @enum_kw, @enum_map_string)
-        end
-
-        # Reflection
-        def __enum_map__(), do: @enum_kw
-
-
-        defp check_value!(atom) when is_atom(atom) do
-          unless @enum_kw[atom] do
-            raise EctoEnum.Error, atom
-          end
-        end
-
-        defp check_value!(string) when is_binary(string) do
-          unless @enum_map_string[string] do
-            raise EctoEnum.Error, string
-          end
-        end
-
-        defp check_value!(int) when is_integer(int) do
-          unless @enum_map[int] do
-            raise EctoEnum.Error, int
-          end
-        end
+        enum @enum_kw
       end
     end
   end
 
-  def cast(atom, _enum_map) when is_atom(atom), do: {:ok, atom}
 
-  def cast(string, _enum_map) when is_binary(string), do: {:ok, String.to_atom(string)}
-
-  def cast(int, enum_map) when is_integer(int) do
-    atom = enum_map[int]
-    {:ok, atom}
+  def cast(term, enum_map) do
+    case to_atom(term, enum_map) do
+      :error -> :error
+      value  -> {:ok, value}
+    end
   end
 
-  def cast(_term), do: :error
-
-  def dump(int, _enum_kw, _enum_map_string) when is_integer(int) do
-    {:ok, int}
+  def load(term, enum_map) do
+    case to_atom(term, enum_map) do
+      :error -> :error
+      value  -> {:ok, value}
+    end
   end
 
-  def dump(atom, enum_kw, _enum_map_string) when is_atom(atom) do
-    {:ok, enum_kw[atom]}
-  end
-
-  def dump(string, _enum_kw, enum_map_string) when is_binary(string) do
-    {:ok, enum_map_string[string]}
+  def dump(term, enum_map) do
+    case to_integer(term, enum_map) do
+      :error -> raise_error term
+      value  -> {:ok, value}
+    end
   end
 
   def dump(_), do: :error
+
+  @doc """
+  Fetch value (integer) from string or atom
+  Fetch values (list of integer) from list of strings or atoms
+  """
+  def to_integer(term, enum_map) when is_atom(term), do: to_integer(to_string(term), enum_map)
+
+  def to_integer(term, enum_map) when is_bitstring(term) do
+    Map.get(enum_map, term, :error)
+  end
+
+  def to_integer(term, enum_map) when is_integer(term) do
+    case Map.get(enum_map, term, :error) do
+      :error -> :error
+      _ -> term
+    end
+  end
+
+  @doc """
+  Fetch name (string or atom) from integer
+  Fetch names (list of strings or atoms) from list of integers
+  """
+  def to_atom(term, enum_map) when is_integer(term) do
+    case Map.get(enum_map, term, :error) do
+      :error -> :error
+      string -> String.to_atom(string)
+    end
+  end
+
+  def to_atom(value, enum_map) when is_atom(value), do: to_atom(to_string(value), enum_map)
+
+  def to_atom(value, enum_map) when is_bitstring(value) do
+    case Map.has_key?(enum_map, value) do
+      true -> String.to_atom(value)
+      _ -> :error
+    end
+  end
+
+  def enums_to_map([{_, _} | _] = keyword) do
+    keyword
+    |> Enum.reduce(%{}, fn({term, ix}, acc) ->
+      unless is_integer(ix) do
+        raise ArgumentError, "#{inspect ix} is not an integer index integer. (#{inspect keyword})"
+      end
+      map = Enum.into([
+        {to_string(term), ix},
+        {ix, to_string(term)}
+      ], %{})
+
+      acc |> Map.merge(map)
+    end)
+  end
+
+  def enums_to_map(list) do
+    list
+    |> Enum.with_index
+    |> enums_to_map
+  end
+
+  def raise_error(term) do
+    raise EctoEnum.Error, term
+  end
+
 end
--- test/ecto_enum_test.exs
+++ test/ecto_enum_test.exs
@@ -2,8 +2,20 @@ defmodule EctoEnumTest do
   use ExUnit.Case
 
   import Ecto.Changeset
-  import EctoEnum
-  defenum StatusEnum, registered: 0, active: 1, inactive: 2, archived: 3
+  import EctoEnum, only: [defenum: 2]
+
+  @valid_enum_map %{
+    0 => "registered", "registered" => 0,
+    1 => "active"    , "active"     => 1,
+    2 => "inactive"  , "inactive"   => 2,
+    3 => "archived"  , "archived"   => 3,
+  }
+
+  defmodule StatusEnum do
+    use EctoEnum
+
+    enum ~w(registered active inactive archived)
+  end
 
   defmodule User do
     use Ecto.Schema
@@ -47,40 +59,76 @@ defmodule EctoEnumTest do
     assert changes.status == :inactive
   end
 
+  test "should chanset errors when input is not in the enum map" do
+    error = {:status, "is invalid"}
+
+    changeset = cast(%User{}, %{"status" => "retroactive"}, ~w(status), [])
+    assert changeset.changes == %{}
+    assert error in changeset.errors
+
+    changeset = cast(%User{}, %{"status" => :retroactive}, ~w(status), [])
+    assert changeset.changes == %{}
+    assert error in changeset.errors
+
+    changeset = cast(%User{}, %{"status" => 4}, ~w(status), [])
+    assert changeset.changes == %{}
+    assert error in changeset.errors
+  end
+
   test "raises when input is not in the enum map" do
-    assert_raise Elixir.EctoEnum.Error, fn ->
-      cast(%User{}, %{"status" => "retroactive"}, ~w(status), [])
-    end
-
-    assert_raise Elixir.EctoEnum.Error, fn ->
-      cast(%User{}, %{"status" => :retroactive}, ~w(status), [])
-    end
-
-    assert_raise Elixir.EctoEnum.Error, fn ->
-      cast(%User{}, %{"status" => 4}, ~w(status), [])
-    end
-
-    assert_raise Elixir.EctoEnum.Error, fn ->
+    assert_raise EctoEnum.Error, fn ->
       TestRepo.insert!(%User{status: "retroactive"})
     end
 
-    assert_raise Elixir.EctoEnum.Error, fn ->
+    assert_raise EctoEnum.Error, fn ->
       TestRepo.insert!(%User{status: :retroactive})
     end
 
-    assert_raise Elixir.EctoEnum.Error, fn ->
+    assert_raise EctoEnum.Error, fn ->
       TestRepo.insert!(%User{status: 5})
     end
   end
 
   test "reflection" do
-    assert StatusEnum.__enum_map__() == [registered: 0, active: 1, inactive: 2, archived: 3]
+    assert StatusEnum.__enum_map__() == ~w(registered active inactive archived)
   end
 
   test "defenum/2 can accept variables" do
     x = 0
     defenum TestEnum, zero: x
+    assert TestEnum.__enum_map__() == [zero: 0]
   end
-end
 
-# TODO: configure to return either string or atom
+  test "normalize enums to double map" do
+    assert @valid_enum_map == EctoEnum.enums_to_map([registered: 0, active: 1, inactive: 2, archived: 3])
+    assert @valid_enum_map == EctoEnum.enums_to_map(~w(registered active inactive archived))
+    assert @valid_enum_map == EctoEnum.enums_to_map(~w(registered active inactive archived)a)
+  end
+
+  test "should value from string" do
+    assert EctoEnum.to_integer("registered", @valid_enum_map) == 0
+    assert EctoEnum.to_integer("active"    , @valid_enum_map) == 1
+    assert EctoEnum.to_integer("inactive"  , @valid_enum_map) == 2
+    assert EctoEnum.to_integer("archived"  , @valid_enum_map) == 3
+    assert EctoEnum.to_integer("_invalid"  , @valid_enum_map) == :error
+    assert EctoEnum.to_integer("Active"    , @valid_enum_map) == :error
+  end
+
+  test "should value from atom" do
+    assert EctoEnum.to_integer(:registered, @valid_enum_map) == 0
+    assert EctoEnum.to_integer(:active    , @valid_enum_map) == 1
+    assert EctoEnum.to_integer(:inactive  , @valid_enum_map) == 2
+    assert EctoEnum.to_integer(:archived  , @valid_enum_map) == 3
+    assert EctoEnum.to_integer(:_invalid  , @valid_enum_map) == :error
+    assert EctoEnum.to_integer(:Active    , @valid_enum_map) == :error
+  end
+
+  test "should atom from integer" do
+    assert EctoEnum.to_atom(0, @valid_enum_map) == :registered
+    assert EctoEnum.to_atom(1, @valid_enum_map) == :active
+    assert EctoEnum.to_atom(2, @valid_enum_map) == :inactive
+    assert EctoEnum.to_atom(3, @valid_enum_map) == :archived
+    assert EctoEnum.to_atom(4, @valid_enum_map) == :error
+  end
+
+end
