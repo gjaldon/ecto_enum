becac0e1b07295ebe62ba875f6a83f91b33f11c1
Karl <karl@theangryangel.co.uk>
2017-08-23 10:46:08 +0100

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -31,7 +31,7 @@ defmodule EctoEnum.Mixfile do
   end
 
   defp deps do
-    [{:ecto, "~> 2.1.0"},
+    [{:ecto, "~> 2.0"},
      {:postgrex, "~> 0.13.0", optional: true},
      {:mariaex, "~> 0.8.0", optional: true},
      {:ex_doc, "~> 0.11", only: :dev},
