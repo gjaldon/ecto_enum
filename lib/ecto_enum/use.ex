2c19ea1974d60203a47c438c328b810562eac9c1
Manuel Rubio <manuel@altenwald.com>
2020-07-02 01:59:21 +0200

    bugfix string enum and implement cast!/1

--- lib/ecto_enum/use.ex
+++ lib/ecto_enum/use.ex
@@ -5,6 +5,20 @@ defmodule EctoEnum.Use do
 
   defmacro __using__(opts) do
     quote bind_quoted: [opts: opts] do
+      [h | _t] = opts
+
+      opts =
+        cond do
+          Keyword.keyword?(opts) ->
+            opts
+
+          is_binary(h) ->
+            Enum.map(opts, fn value -> {String.to_atom(value), value} end)
+
+          true ->
+            raise "Enum must be a keyword list or a list of strings"
+        end
+
       typespec = Typespec.make(Keyword.keys(opts))
 
       @behaviour Ecto.Type
@@ -31,6 +45,12 @@ defmodule EctoEnum.Use do
       end
 
       def cast(_other), do: :error
+
+      for {key, value} <- opts, k <- Enum.uniq([key, value, Atom.to_string(key)]) do
+        def cast!(unquote(k)), do: unquote(key)
+      end
+
+      def cast!(other), do: raise Ecto.CastError, type: __MODULE__, value: other
 
       for {key, value} <- opts, k <- Enum.uniq([key, value, Atom.to_string(key)]) do
         def dump(unquote(k)), do: {:ok, unquote(value)}
